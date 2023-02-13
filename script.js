59ef80f1acd0321ce1d347386086f658751f460d
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-02-13 13:43:43 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 /*
   Public api url
 */
+const url = "https://gutendex.com/";
+
+async function getBooks() {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+}
+
+/*
+async function getCharacters() {
+  loader.classList.add("show");
+  const response = await fetch(url + "s?page=" + page);
+  const result = await response.json();
+  console.log(result);
+
+  result.data.forEach((character) => addCharacter(character));
+  loader.classList.remove("show");
+}
+*/
