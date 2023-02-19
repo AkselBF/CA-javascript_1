7c1b4631829c92d75531f1214f21716058763840
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-02-19 18:56:28 +0100

    updates

--- javascript/details.js
+++ javascript/details.js
@@ -1,6 +1,38 @@
 console.log(localStorage.getItem("id"));
-let id;
+//let id;
+const detailContainer = document.querySelector(".book_details");
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const bookId = params.get("id");
 
+//const img = document.createElement("img");
+
+const url = "https://gutendex.com/books?ids=" + bookId;
+//const url = `https://gutendex.com/books?ids=${bookId}`;
+//console.log(url);
+
+async function fetchBook() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  createHTML(data);
+}
+
+fetchBook();
+
+function createHTML(data) {
+  detailContainer.innerHTML = `
+  <h1 class="specific_book_title">${data.title}</h1>
+  <div class="details_image"
+    style="background-image: url('${data.formats}')">
+  </div>
+  <div class="details_author">
+    ${data.authors}
+  </div>`;
+}
+
+/*
 async function getBooks() {
   const response = await fetch(`https://gutendex.com/books?ids=${id}`);
   const data = await response.json();
@@ -8,3 +40,4 @@ async function getBooks() {
 }
 
 getBooks();
+*/
